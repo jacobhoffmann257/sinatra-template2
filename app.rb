852219114593c16119a9ef492a6ea6ee35e9086b
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-09-14 20:35:02 +0000

    parsed responses

--- app.rb
+++ app.rb
@@ -15,6 +15,21 @@ end
 get("/search") do
   erb(:search)
 end
-post("/bookresult") do 
+post("/bookresult") do
+isbn = params.fetch("isbn")
+libraryurl = "https://openlibrary.org/works/#{isbn}.json"
+rawresponse = HTTP.get(libraryurl)
+parsedresponse = JSON.parse(rawresponse)
+@title = parsedresponse.fetch("title")
+@author = parsedresponse.fetch("authors")
+@description = parsedresponse.fetch("description")
+@places = parsedresponse.fetch("subject_places")
+@subjects = parsedresponse.fetch("subjects")
+@people = parsedresponse.fetch("subject_people")
+@times = parsedresponse.fetch("subject_times")
+@location = parsedresponse.fetch("location")
+@created = parsedresponse.fetch("created")
+
+puts @places
 erb(:bookresult)
 end
