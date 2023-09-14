604535ffeaac044c7077cd9a647b4f3ed2f4498e
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-09-14 20:18:44 +0000

    basics setup

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "sinatra/cookies"
+require "json"
+require "better_errors"
+require "binding_of_caller"
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+get("/search") do
+  erb(:search)
+end
+post("/bookresult") do 
+erb(:bookresult)
 end
