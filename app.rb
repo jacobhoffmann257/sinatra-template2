8c362f673ec84e5d9b96b621be600e3bab7f471f
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-09-20 18:33:05 +0000

    final

--- app.rb
+++ app.rb
@@ -5,9 +5,7 @@ require "sinatra/cookies"
 require "json"
 require "better_errors"
 require "binding_of_caller"
-use(BetterErrors::Middleware)
-BetterErrors.application_root = __dir__
-BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
 
 get("/") do
   erb(:home)
